fe3241ce2e4a7d5c1de5de74a9236aceda333bda
mikefront <mike_front@mail.ru>
2023-08-17 18:22:27 +0500

    Задача1 выполнена

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,26 @@ public class Main {
     }
     public static void task1() {
         System.out.println("Задача1");
-
+        {
+        }
+        byte a = 4;
+        System.out.println("a- " + a);
+        {
+        }
+        short b = 34;
+        System.out.println("b- " + b);
+        {
+        }
+        int c = 456;
+        System.out.println("c- " +c);
+        {
+        }
+        float f = 27.7f;
+        System.out.println("f- " + f);
+        {
+        }
+        double d = 456.456987465;
+        System.out.println("d- " + d);
     }
 
 }
