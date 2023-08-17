72f1d511c2b38788aeb6d63895eef54773a5b8b4
mikefront <mike_front@mail.ru>
2023-08-17 18:00:51 +0500

    Задача1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача1");
+
+    }
+
+}
